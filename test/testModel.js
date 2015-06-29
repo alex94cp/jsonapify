68ec4b7bf600ffe35baf32b35f9d80a2f038e943
alex94puchades <alex94puchades@gmail.com>
2015-06-29 03:39:59 +0200

    Major redesign

--- test/testModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var testModelSchema = new mongoose.Schema({
-	string: String,
-	number: Number,
-	boolean: Boolean,
-});
-
-module.exports = mongoose.model('TestModel', testModelSchema);
