6c088dc9f698e8e237a997947477537f6930007d
alex94puchades <alex94puchades@gmail.com>
2015-06-29 19:27:21 +0200

    Added rw-field unit tests

--- test/field.js
+++ test/field.js
@@ -18,6 +18,18 @@ describe('Field', function() {
 				done();
 			});
 		});
+		
+		it('omits not-readable fields from document object', function(done) {
+			var output = {};
+			var response = new Response;
+			var object = new TestModel({ string: 'string' });
+			var field = new Field('string', { readable: false });
+			field.serialize(object, response, function(err, value) {
+				if (err) return done(err);
+				expect(value).to.not.exist;
+				done();
+			});
+		});
 	});
 	
 	describe('#deserialize', function() {
@@ -28,6 +40,17 @@ describe('Field', function() {
 			field.deserialize('string', response, output, function(err) {
 				if (err) return done(err);
 				expect(output).to.have.property('string', 'string');
+				done();
+			});
+		});
+		
+		it('does not set not-writable fields into resource object', function(done) {
+			var output = new TestModel;
+			var response = new Response;
+			var field = new Field('string', { writable: false });
+			field.deserialize('string', response, output, function(err) {
+				if (err) return done(err);
+				expect(output).to.have.property('string', undefined);
 				done();
 			});
 		});
