028bc97b0f5ba83e16f6d3d01a6aa0649e662d5d
alex94puchades <alex94puchades@gmail.com>
2015-06-29 19:16:22 +0200

    Added const serializers with unit tests

---
+++ test/const.js
@@ -0,0 +1,31 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var Const = require('../lib/const');
+var Response = require('../lib/response');
+var TestModel = require('./testModel');
+
+describe('Const', function() {
+	describe('#serialize', function() {
+		it('sets resource property to value', function(done) {
+			var output = {};
+			var object = new TestModel;
+			var response = new Response;
+			var constant = new Const('value');
+			constant.serialize(object, response, function(err, value) {
+				if (err) return done(err);
+				expect(value).to.equal('value');
+				done();
+			});
+		});
+	});
+	
+	describe('#deserialize', function() {
+		it('does not change anything in document object', function(done) {
+			var output = new TestModel;
+			var response = new Response;
+			var constant = new Const('value');
+			constant.deserialize('value', response, output, done);
+		});
+	});
+});
