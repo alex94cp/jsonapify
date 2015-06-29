38442c8893dba8ad198b8c5b059c02b8849ae079
alex94puchades <alex94puchades@gmail.com>
2015-06-29 19:09:46 +0200

    Added support for field access specifiers

--- test/field.js
+++ test/field.js
@@ -2,26 +2,19 @@ var chai = require('chai');
 var expect = chai.expect;
 
 var Field = require('../lib/field');
-var Resource = require('../lib/resource');
 var Response = require('../lib/response');
 var TestModel = require('./testModel');
 
 describe('Field', function() {
-	var resource;
-	before(function() {
-		resource = new Resource(TestModel, {
-			field: new Field('string'),
-		});
-	});
-	
 	describe('#serialize', function() {
 		it('sets resource property from document object field', function(done) {
 			var output = {};
-			var object = new TestModel({ string: 'string' });
 			var response = new Response;
-			resource.serialize(object, response, output, function(err) {
+			var object = new TestModel({ string: 'string' });
+			var field = new Field('string');
+			field.serialize(object, response, function(err, value) {
 				if (err) return done(err);
-				expect(output).to.have.property('field', object.string);
+				expect(value).to.equal(object.string);
 				done();
 			});
 		});
@@ -30,11 +23,11 @@ describe('Field', function() {
 	describe('#deserialize', function() {
 		it('sets document object field from resource property', function(done) {
 			var output = new TestModel;
-			var resdata = { field: 'string' };
 			var response = new Response;
-			resource.deserialize(resdata, response, output, function(err) {
+			var field = new Field('string');
+			field.deserialize('string', response, output, function(err) {
 				if (err) return done(err);
-				expect(output).to.have.property('string', resdata.field);
+				expect(output).to.have.property('string', 'string');
 				done();
 			});
 		});
