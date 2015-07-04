68bdfc3b079db99c123e1c36acc4f6c08c573ae9
alex94puchades <alex94puchades@gmail.com>
2015-07-04 16:58:47 +0200

    Added read middleware with unit tests

--- test/read.js
+++ test/read.js
@@ -1,6 +1,5 @@
 var _ = require('lodash');
 var chai = require('chai');
-var async = require('async');
 var httpMocks = require('node-mocks-http');
 var expect = chai.expect;
 
@@ -11,8 +10,8 @@ var mongoose = require('mongoose');
 var jsonapify = require('../');
 var Resource = require('../lib/resource');
 
-describe('enumerate', function() {
-	var TestModel, resource, enumerate;
+describe('read', function() {
+	var TestModel, resource, read;
 	before(function(done) {
 		mongoose.connect('mongodb://localhost/test', function(err) {
 			if (err) return done(err);
@@ -21,7 +20,7 @@ describe('enumerate', function() {
 				type: 'testmodels',
 				id: jsonapify.field('_id'),
 			});
-			enumerate = jsonapify.enumerate(resource);
+			read = jsonapify.read(resource);
 			done();
 		});
 	});
@@ -36,42 +35,34 @@ describe('enumerate', function() {
 	});
 	
 	it('sends expected json-api response', function(done) {
-		async.parallel([
-			TestModel.create.bind(TestModel, {}),
-			TestModel.create.bind(TestModel, {}),
-			TestModel.create.bind(TestModel, {}),
-		], function(err, results) {
+		TestModel.create({}, function(err, object) {
 			if (err) return done(err);
-			var req = httpMocks.createRequest();
+			var req = httpMocks.createRequest({ params: { id: object._id }});
 			var res = httpMocks.createResponse();
-			enumerate(req, res, function(err) {
+			read(req, res, function(err) {
 				if (err) return done(err);
-				var count = results.length;
 				expect(res.statusCode).to.equal(200);
 				var resdata = JSON.parse(res._getData());
 				expect(resdata).to.have.property('data');
-				expect(resdata.data).to.have.length(count);
-				expect(resdata).to.have.deep.property('meta.count', count);
-				for (var i = 0; i < count; ++i) {
-					var match = { type: 'testmodels', id: results[i].id };
-					expect(resdata.data).to.include(match);
-				}
+				expect(resdata.data).to.have.property('id');
+				expect(resdata.data).to.have.property('type', 'testmodels');
+				expect(resdata.data.id).to.satisfy(function(id) {
+					return object._id.equals(id);
+				});
 				done();
 			});
 		});
 	});
 	
-	it('sends empty array if no resources', function(done) {
-		var req = httpMocks.createRequest();
+	it('sends null if resource not found', function(done) {
+		var oid = mongoose.Types.ObjectId();
+		var req = httpMocks.createRequest({ params: { id: oid }});
 		var res = httpMocks.createResponse();
-		enumerate(req, res, function(err) {
+		read(req, res, function(err) {
 			if (err) return done(err);
-			expect(res.statusCode).to.equal(200);
+			expect(res.statusCode).to.equal(404);
 			var resdata = JSON.parse(res._getData());
-			expect(resdata).to.have.property('data');
-			expect(resdata).to.have.deep.property('meta.count', 0);
-			expect(resdata.data).to.be.an.instanceOf(Array);
-			expect(resdata.data).to.have.length(0);
+			expect(resdata).to.have.property('data', null);
 			done();
 		});
 	});
