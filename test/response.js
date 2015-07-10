76de4730af9cb73ea0deb02658824ddd14fd167f
alex94puchades <alex94puchades@gmail.com>
2015-07-10 14:53:39 +0200

    Improved response interface

--- test/response.js
+++ test/response.js
@@ -6,17 +6,27 @@ var Response = require('../lib/response');
 describe('Response', function() {
 	var response;
 	beforeEach(function() {
-		response = Response.create();
+		response = new Response;
 	});
 	
 	describe('#meta', function() {
-		it('sets name in meta object as value', function() {
+		it('sets name in meta object as value in response', function() {
 			response.meta('name', 'value')
 			var data = response.toJSON();
-			expect(data).to.have.deep.property('meta.name', 'value');
+			var expected = { name: 'value' };
+			expect(data).to.have.property('meta');
+			expect(data.meta).to.deep.equal(expected);
 		});
 		
-		it('returns name in meta object', function() {
+		it('replaces whole meta object in response', function() {
+			var expected = { name: 'value' };
+			response.meta(expected);
+			var data = response.toJSON();
+			expect(data).to.have.property('meta');
+			expect(data.meta).to.deep.equal(expected);
+		});
+		
+		it('returns name from meta object in response', function() {
 			response.meta('name', 'value');
 			var value = response.meta('name');
 			expect(value).to.equal('value');
@@ -24,24 +34,61 @@ describe('Response', function() {
 	});
 	
 	describe('#link', function() {
-		it('sets name in links object as url', function() {
+		it('sets name in links object as url in response', function() {
 			response.link('name', 'url')
 			var data = response.toJSON();
-			expect(data).to.have.deep.property('links.name', 'url');
+			var expected = { name: 'url' };
+			expect(data).to.have.property('links');
+			expect(data.links).to.deep.equal(expected);
 		});
 		
-		it('returns name in links object', function() {
+		it('returns name from links object in response', function() {
 			response.link('name', 'url');
 			var value = response.link('name');
 			expect(value).to.equal('url');
 		});
 	});
 	
+	describe('#links', function() {
+		it('replaces whole links object in response', function() {
+			var expected = { name: 'value' }
+			response.links(expected);
+			var data = response.toJSON();
+			expect(data).to.have.property('links');
+			expect(data.links).to.deep.equal(expected);
+		});
+		
+		it('returns whole links object from response', function() {
+			var expected = { name: 'value' };
+			response.links(expected);
+			var links = response.links();
+			expect(links).to.deep.equal(expected);
+		});
+	});
+	
 	describe('#error', function() {
-		it('adds error to errors object', function() {
+		it('adds error to errors object in response', function() {
 			response.error('error');
 			var data = response.toJSON();
-			expect(data).to.have.property('errors').which.includes('error');
+			expect(data).to.have.property('errors');
+			expect(data.errors).to.include('error');
+		});
+	});
+	
+	describe('#errors', function() {
+		it('replaces whole errors array in response', function() {
+			var expected = ['error0', 'error1'];
+			response.errors(expected);
+			var data = response.toJSON();
+			expect(data).to.have.property('errors');
+			expect(data.errors).to.deep.equal(expected);
+		});
+		
+		it('returns whole errors array from response', function() {
+			var expected = ['error0', 'error1'];
+			response.errors(expected);
+			var errors = response.errors();
+			expect(errors).to.deep.equal(expected);
 		});
 	});
 	
@@ -64,7 +111,7 @@ describe('Response', function() {
 			expect(data).to.have.property('data').which.includes(1,2,3);
 		});
 		
-		it('returns data object', function() {
+		it('returns data object from response', function() {
 			response.data('data');
 			var data = response.data();
 			expect(data).to.equal('data');
@@ -72,18 +119,36 @@ describe('Response', function() {
 	});
 	
 	describe('#include', function() {
-		it('sets include to included object', function() {
+		it('adds include to included objects in response', function() {
 			var expected = { type: 'type', id: 1, field: 'field' };
 			response.include(expected.type, expected.id, expected);
 			var data = response.toJSON();
-			expect(data).to.have.property('included').which.includes(expected);
+			expect(data).to.have.property('included');
+			expect(data.included).to.include(expected);
 		});
 		
-		it('returns included object', function() {
+		it('returns included object from response', function() {
 			var expected = { type: 'type', id: 1, field: 'field' };
 			response.include(expected.type, expected.id, expected);
 			var include = response.include(expected.type, expected.id);
 			expect(include).to.deep.equal(expected);
+		});
+	});
+	
+	describe('#includes', function() {
+		it('replaces all includes in response', function() {
+			var expected = [{ type: 'type', id: 1, field: 'field' }];
+			response.includes(expected);
+			var data = response.toJSON();
+			expect(data).to.have.property('included');
+			expect(data.included).to.deep.equal(expected);
+		});
+		
+		it('returns all includes from response', function() {
+			var expected = [{ type: 'type', id: 1, field: 'field' }];
+			response.includes(expected);
+			var includes = response.includes();
+			expect(includes).to.deep.equal(expected);
 		});
 	});
 });
