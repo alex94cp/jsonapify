803a9a816b97e3c1a6057af39c4d806a79c62777
alex94puchades <alex94puchades@gmail.com>
2015-07-25 02:29:24 +0200

    Added paginate filter with unit tests

---
+++ test/filters/paginate.js
@@ -0,0 +1,114 @@
+var _ = require('lodash');
+var chai = require('chai');
+var async = require('async');
+chai.use(require('sinon-chai'));
+var mongoose = require('mongoose');
+var httpMocks = require('node-mocks-http');
+var expect = chai.expect;
+
+var common = require('../common');
+var jsonapify = require('../../');
+var Resource = jsonapify.Resource;
+var Response = jsonapify.Response;
+var Transaction = jsonapify.Transaction;
+var paginate = jsonapify.filters.paginate;
+
+describe('paginate', function() {
+	var model, resource, transaction, objects;
+	before(function(done) {
+		mongoose.connect('mongodb://localhost/test', function(err) {
+			if (err) return done(err);
+			model = mongoose.model('PaginateTest', new mongoose.Schema);
+			done();
+		});
+	});
+	
+	beforeEach(function(done) {
+		resource = new Resource(model, { type: 'test' });
+		async.parallel([
+			function(next) { model.create({}, next); },
+			function(next) { model.create({}, next); },
+			function(next) { model.create({}, next); },
+			function(next) { model.create({}, next); },
+			function(next) { model.create({}, next); },
+		], function(err, results) {
+			if (err) return done(err);
+			var res = httpMocks.createResponse();
+			var response = new Response(res);
+			transaction = new Transaction(resource, response);
+			objects = results;
+			done();
+		});
+	});
+	
+	afterEach(function(done) {
+		mongoose.connection.db.dropDatabase(done);
+	});
+	
+	after(function(done) {
+		mongoose.disconnect(done);
+	});
+	
+	it('paginates resources and adds pagination links', function(done) {
+		paginate()(transaction);
+		var req = httpMocks.createRequest({
+			query: { page: { number: 2, size: 2 }}
+		});
+		transaction.notify(resource, 'start', req);
+		var resview = resource.view(transaction);
+		resview.findMany({}, function(err, results) {
+			if (err) return done(err);
+			expect(results).to.have.length(2);
+			var response = transaction.response;
+			response.meta['count'] = objects.length;
+			transaction.notify(resource, 'end');
+			expect(response).to.have.deep.property('links.first');
+			expect(response).to.have.deep.property('links.last');
+			expect(response).to.have.deep.property('links.prev');
+			expect(response).to.have.deep.property('links.next');
+			done();
+		});
+	});
+	
+	it('omits prev link if first page selected', function(done) {
+		paginate()(transaction);
+		var req = httpMocks.createRequest({
+			query: { page: { number: 1, size: 2 }}
+		});
+		transaction.notify(resource, 'start', req);
+		var resview = resource.view(transaction);
+		resview.findMany({}, function(err, results) {
+			if (err) return done(err);
+			expect(results).to.have.length(2);
+			var response = transaction.response;
+			response.meta['count'] = objects.length;
+			transaction.notify(resource, 'end');
+			expect(response).to.have.deep.property('links.first');
+			expect(response).to.have.deep.property('links.last');
+			expect(response).to.not.have.deep.property('links.prev');
+			expect(response).to.have.deep.property('links.next');
+			done();
+		});
+	});
+	
+	it('omits next link if last page selected', function(done) {
+		paginate()(transaction);
+		var req = httpMocks.createRequest({
+			query: { page: { number: 3, size: 2 }}
+		});
+		transaction.notify(resource, 'start', req);
+		var resview = resource.view(transaction);
+		resview.findMany({}, function(err, results) {
+			if (err) return done(err);
+			expect(results).to.have.length.of.most(2);
+			var response = transaction.response;
+			response.meta['count'] = objects.length;
+			transaction.notify(resource, 'end');
+			expect(response).to.have.deep.property('links.first');
+			expect(response).to.have.deep.property('links.last');
+			expect(response).to.have.deep.property('links.prev');
+			expect(response).to.not.have.deep.property('links.next');
+			done();
+		});
+	});
+});
